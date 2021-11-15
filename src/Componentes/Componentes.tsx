c1dd7f163e40240e989e05075f3156bd53ef330a
k-1-e <obregonkevin1351@gmail.com>
2021-11-15 09:36:12 -0600

    terminado app para restaurantes

--- src/Componentes/Componentes.tsx
+++ src/Componentes/Componentes.tsx
@@ -2,6 +2,7 @@ import React, { useState,useEffect } from 'react'
 import { View, Text, ScrollView, StyleSheet, TextInput, TouchableOpacity, Button } from 'react-native';
 import {FontAwesome} from '@expo/vector-icons';
 
+
 const Componentes = () => {
     const [total, setTotal] = useState<string>('400')
     const [personas, setPersonas] = useState('4')
@@ -220,6 +221,104 @@ const styles = StyleSheet.create({
         borderWidth: 0,
         marginBottom: 5,
         marginTop: 5
+    },
+    inputs: {
+        backgroundColor: '#F2F8FB',
+        borderRadius: 8,
+        padding: 10,
+        textAlign: 'right',
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#004445'
+    },
+    textaddon: {
+        position: 'absolute',
+        borderRadius: 8,
+        padding: 10,
+        textAlign: 'right',
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#004445'
+
+    },
+    boxSelectTip: {
+        marginTop: 25
+    },
+    containersButtons: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        alignContent: "space-between",
+        flexWrap: 'wrap'
+    },
+    botonPorcentaje: {
+        backgroundColor: '#00494C',
+        borderRadius: 8,
+        width: '48%',
+        paddingVertical: 7,
+        marginTop: 10
+    },
+    botonPorcentajeActivate: {
+        backgroundColor: '#00C1AC',
+        borderRadius: 8,
+        width: '48%',
+        paddingVertical: 7,
+        marginTop: 10
+    },
+    botonReset: {
+        backgroundColor: '#00C1AC',
+        borderRadius: 8,
+        width: '100%',
+        paddingVertical: 7,
+        marginTop: 1
+    },
+    textbotonPorcentaje: {
+        color: '#F5FBFC',
+        fontWeight: 'bold',
+        fontSize: 35,
+        textAlign: 'center'
+    },
+    textbotonPorcentajeActivate: {
+        color: '#004B4F',
+        fontWeight: 'bold',
+        fontSize: 35,
+        textAlign: 'center'
+    },
+    textbotonReset: {
+        color: '#00514E',
+        fontWeight: 'bold',
+        fontSize: 35,
+        textAlign: 'center'
+    },
+    boxResumen: {
+        backgroundColor: '#00494C',
+        borderRadius: 8,
+        marginTop: 25,
+        padding: 20
+    },
+    textWhiteHeaderText: {
+        color: '#EDFEFE',
+        fontWeight: 'bold',
+        fontSize: 22,
+        borderWidth: 0
+    },
+    textGrayResumen: {
+        color: '#4F8B8E',
+        fontWeight: 'bold',
+        fontSize: 14,
+        borderWidth: 0
+    },
+    textMontoResumen: {
+        color: '#00BEAD',
+        fontWeight: 'bold',
+        fontSize: 40,
+        borderWidth: 0
+    },
+    filaResumen: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20
     }
 
 })
